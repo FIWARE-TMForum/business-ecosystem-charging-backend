640a7b00ed0c9538ddfdb0e97628b750f248f98c
Francisco de la Vega <fdelavega@ficodes.com>
2026-02-26 12:16:48 +0100

    Use tax rate instead of full value (#131)
    * Use tax rate instead of full value
    
    * Add tests to validate tax rate format
    
    * Test the environment in python 3.10
    
    * Try with a different test runner for python 3.10
    
    * Keep python 3.10 with a compatibility check

--- src/wstore/charging_engine/charging/billing_client.py
+++ src/wstore/charging_engine/charging/billing_client.py
@@ -83,6 +83,8 @@ class BillingClient:
                 raise
 
     def create_customer_rate(self, rate_type, currency, tax_rate, tax, tax_included, tax_excluded, billing_account, product_id, coverage_period=None, party=[]):
+        raw_rate = Decimal(str(tax_rate))
+        decimal_rate = raw_rate / Decimal("100") if raw_rate > Decimal("1") else raw_rate
         data = {
             # "appliedBillingRateType": rate_type,
             "name": "INITIAL PAYMENT",
@@ -92,7 +94,7 @@ class BillingClient:
             "date": datetime.datetime.utcnow().replace(microsecond=0).isoformat() + "Z",
             "appliedTax": [{
                 "taxCategory": "VAT",
-                "taxRate": tax_rate,
+                "taxRate": decimal_rate,
                 "taxAmount": {
                     "unit": currency,
                     "value": tax
